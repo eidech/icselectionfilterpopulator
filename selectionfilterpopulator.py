829cd205813340eaaec0c22de29a9a3ab2f647e0
eidech <christopher.eide@gmail.com>
2023-02-09 10:09:42 -0500

    Removed unused imports

--- selectionfilterpopulator.py
+++ selectionfilterpopulator.py
@@ -6,10 +6,8 @@
 #############################################################
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
 import csv
 
 # GLOBAL CONSTANTS (CONFIG)
